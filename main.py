bd1e5317732056bf1d9704d5cf3245e9938ed341
madeyexz <ian.xiao@stu.pku.edu.cn>
2023-04-10 15:00:47 +0800

    main.py is now workable, some trivial problems would be fixed soon

---
+++ main.py
@@ -0,0 +1,81 @@
+import sys
+import os
+import whisper
+import json # used to visualize output
+from typing import Iterator, TextIO
+
+def vid_to_audio(file):
+    '''extract audio from video using ffmpeg'''
+    os.sys("ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn audio.mp3".format(file))
+    return
+
+def get_working_dir():
+    '''get working directory'''
+    working_dir = os.getcwd()
+    return working_dir
+
+def whisper_transcribe(file="{}/audio.mp3".format(dir)):
+    '''transcribe audio to text using whisper'''
+    model = whisper.load_model("base")
+    result = model.transcribe(file)
+    json_object = json.dumps(result, indent=4)
+    return result, json_object
+
+def whisper_result_preview_json(json_object):
+    '''useful for debugging, preview the result in json format'''
+    with open("result.json", "w") as f:
+        f.write(json_object)
+    return
+
+def format_timestamp(seconds: float, always_include_hours: bool = False):
+    '''format timestamp to SRT format'''
+    assert seconds >= 0, "non-negative timestamp expected"
+    milliseconds = round(seconds * 1000.0)
+
+    hours = milliseconds // 3_600_000
+    milliseconds -= hours * 3_600_000
+
+    minutes = milliseconds // 60_000
+    milliseconds -= minutes * 60_000
+
+    seconds = milliseconds // 1_000
+    milliseconds -= seconds * 1_000
+
+    hours_marker = f"{hours}:" if always_include_hours or hours > 0 else ""
+    return f"{hours_marker}{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
+
+def write_srt(transcript: Iterator[dict], file: TextIO):
+    '''write transcript to SRT file'''
+    for i, segment in enumerate(transcript, start=1):
+        print(
+            f"{i}\n"
+            f"{format_timestamp(segment['start'], always_include_hours=True)} --> "
+            f"{format_timestamp(segment['end'], always_include_hours=True)}\n"
+            f"{segment['text'].strip().replace('-->', '->')}\n",
+            file=file,
+            flush=True,
+        )
+
+
+def whisper_result_to_srt(whisper_result):
+    '''convert whisper result to SRT format'''
+    with open("subtitle.srt", "w", encoding="utf-8") as srt:
+        write_srt(whisper_result["segments"], file=srt)
+    return
+
+
+
+if __name__ == "__main__":
+    '''main function'''
+    
+    
+    # file = sys.argv[1]
+    # file = "/Users/ianhsiao/Documents/AI_learning/Whisper_Subtitle/sample_audio/sample1.flac"
+    file = "/Users/ianhsiao/Documents/AI_learning/Whisper_Subtitle/sample_audio/Surge is Incredibly Good Right Now.mp4"
+    dir = get_working_dir()
+    result, json_object = whisper_transcribe(file)
+    whisper_result_to_srt(result)
+    
+    # whisper_result_preview_json(json_object)
+    
+    # print(result["text"])
